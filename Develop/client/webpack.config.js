2379a5ff61f18a47b239a362ba6bf3c511ef777e
Kyle <kcparks1234@gmail.com>
2023-05-08 19:43:30 -0700

    Added initial webpack configuration. Added all the required plugins for the project, and a description of the project.

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,27 +3,67 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: './client/src/js/index.js',
+      install: './client/src/js/install.js'
     },
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'client', 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './client/index.html',
+        filename: 'index.html',
+        chunks: ['main']
+      }),
+      new HtmlWebpackPlugin({
+        template: './client/install.html',
+        filename: 'install.html',
+        chunks: ['install']
+      }),
+      new WebpackPwaManifest({
+        name: 'Edit Your Text',
+        short_name: 'EYT',
+        description: 'Edit Your Text is a versatile text editor web application that allows users to create, edit, and save content efficiently. It features a client-server folder structure, efficient bundling using Webpack, a generated HTML file, service worker, and a manifest file. The app supports next-gen JavaScript, has integrated IndexedDB for content storage, and can be installed as a desktop icon. It also features a registered service worker using Workbox, pre-caching of static assets, and deployment support on Heroku.',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials',
+        icons: [
+          {
+            src: path.resolve('client/src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './client/src-sw.js',
+        swDest: 'sw.js',
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
       ],
     },
   };
