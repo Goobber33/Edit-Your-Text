eeb93384a1218f8366ee7b1bc2cf0ef1ba0e044b
Kyle <kcparks1234@gmail.com>
2023-05-15 19:03:07 -0700

    Added some changes to my overall code and there are no more errors. I worked through each one, and now my install button wont work.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,32 +1,34 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+const initdb = async () => {
+  return openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (!db.objectStoreNames.contains('jate')) {
+        db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+        console.log('jate database created');
+      } else {
         console.log('jate database already exists');
-        return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
     },
   });
+};
 
-// Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const db = await initdb();
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store.put(content);
+  const response = await store.put({ jate: content });
+  console.log("Data saved to the database", response);
   return tx.done;
 };
 
-// Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const db = await initdb();
   const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  return store.getAll();
+  const result = await store.getAll();
+  console.log(result);
+  return result;
 };
 
 initdb();
--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -26,8 +26,9 @@ export default class {
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
-    });
+      const editorContent = data.length > 0 ? data[0].jate : null;
+      this.editor.setValue(editorContent || localData || header);
+    });    
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
